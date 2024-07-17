a77746ccd011ed6bde7236e92bc43f1004be217f
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2024-07-17 08:27:34 +0300

    wip

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default [defineActionConfig('log')];
 function defineActionConfig(action: string) {
 	return defineConfig({
 		entry: [`actions/${action}/index.ts`],
-		outDir: `actions/${action}`,
+		outDir: `actions/${action}/dist`,
 		format: 'esm',
 	});
 }
