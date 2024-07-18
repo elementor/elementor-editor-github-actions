eb3dec30328358b3bc095285fdd80282e691fc66
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2024-07-18 09:26:11 +0300

    wip

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup';
+import * as fs from 'node:fs/promises';
 
 export default [defineActionConfig('log')];
 
@@ -7,5 +8,23 @@ function defineActionConfig(action: string) {
 		entry: [`actions/${action}/index.ts`],
 		outDir: `actions/${action}/dist`,
 		format: 'esm',
+
+		esbuildPlugins: [
+			{
+				name: 'remove-import-zx',
+				setup(build) {
+					build.onLoad({ filter: /.*/ }, async (args) => {
+						const content = await fs.readFile(args.path, 'utf-8');
+
+						return {
+							contents: content.replaceAll(
+								/^import .+ from 'zx';?$/gm,
+								'',
+							),
+						};
+					});
+				},
+			},
+		],
 	});
 }
