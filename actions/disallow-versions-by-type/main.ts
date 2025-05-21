0eb0a606ba08bacd9b5a6c1e3e87883fa80b0f9e
Omer Haim Israeli <omeri@elementor.com>
2025-05-21 15:09:07 +0300

    wip

--- actions/disallow-versions-by-type/main.ts
+++ actions/disallow-versions-by-type/main.ts
@@ -42,7 +42,9 @@ export async function run() {
 function parseInputs() {
     try {
         const parsed = z.object({
-            disallowedVersions: z.array(z.enum(['major', 'minor', 'patch'])),
+            disallowedVersions: z.array(z.string().refine(val => ['minor', 'major', 'patch'].includes(val), {
+                message: "Version type must be one of: 'minor', 'major', 'patch'"
+            })),
         }).parse({
             disallowedVersions: getArrayInput('disallowed-versions'),
         });
